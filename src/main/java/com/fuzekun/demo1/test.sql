efce56e1848e0f8944acfb66cb10cb85c0bdac50
fuzekun <1078682405@qq.con>
2023-07-13 19:18:52 +0800

    fix：批量操作html文件所需要的依赖。

--- src/main/java/com/fuzekun/demo1/test.sql
+++ src/main/java/com/fuzekun/demo1/test.sql
@@ -1,2 +1,4 @@
-select * from comment where content = '二级评论';
+select * from comment where content = '再来一个回帖';
 select content from discuss_post where id = 241;
+delete  from user where username = 'fuzekun';
+select * from user where username = 'fuzekun';
